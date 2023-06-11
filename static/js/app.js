dcc529ff061f94b3e859a57f123d85c8b2d4f6f9
irinatenis <irinatenis78@yahoo.com>
2023-06-10 19:41:17 -0500

    bar,bubbles,metadata

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,32 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 // Fetch the JSON data and console log it
 d3.json(url).then(function (importedData) {
+  console.log (importedData);
   initMyPage(importedData)
 })
 let globalData = []
 function initMyPage(data) {
     globalData = data
     let otu_ids=data.samples[0]["otu_ids"];
-    let values=data.samples[0]["sample_values"].reverse();
-    let toolTip=data.samples[0]["otu_labels"].reverse();
+    let values=data.samples[0]["sample_values"]
+    let toolTip=data.samples[0]["otu_labels"]
+    let metadata=data.metadata[0]
 
-    let traceData = [{y:otu_ids.map(row=>"OTU "+row),
-      x:values,
+    let traceData = [{y:otu_ids.slice(0,10).reverse().map(row=>"OTU "+row),
+      x:values.slice(0,10).reverse(),
       type:"bar",
+      text: toolTip.slice(0,10).reverse(),
       orientation:'h'
     }];
     // Apply a title to the layout
-      let layout = {title: "Top OTUs"};
+      let layout = {
+        title:{ 
+        text: "Ten Top OTUs",
+        font: {
+          size: 20
+        }
+      }
+    };
     // Render the plot to the div tag with id "plot"
       Plotly.newPlot("bar",traceData, layout);
 
@@ -31,35 +41,40 @@ function initMyPage(data) {
     let traceData2 = [{x:otu_ids,
       y:values,
       mode:"markers",
+      text: toolTip,
       marker: {
         size:values,
         color: otu_ids,
-        // color: [35, 10, 50, 40, 18, 30],
         colorscale: 'YlGnBu',
+        sizeref: 0.03,
         sizemode: 'area'
+       
       }}];
     // Apply a title to the layout
     let layout2 = {
-      title: {
-        text:'Bubble Chart',
         font: {
-          family: 'Courier New, monospace',
-          size: 24
+          family: 'Calibri, monospace',
+          size: 16
         },
-      },
       xaxis: {
         title: {
-          text: 'otu_ids',
+          text: 'OTU ID',
           font: {
-            family: 'Courier New, monospace',
-            size: 18,
-            color: '#7f7f7f'
+            family: 'Calibri, monospace',
+            size: 14
           }
         },
       },
     };
     // Render the plot to the div tag with id "plot"
     Plotly.newPlot("bubble",traceData2, layout2);
+
+    //METADATA
+    console.log(metadata);
+    
+    console.log(`id ${metadata.id}`);
+    info.innerHTML = `id: ${metadata.id} </br> ethnicity: ${metadata.ethnicity} </br> gender: ${metadata.gender} </br> age: ${metadata.age} \
+    </br> location: ${metadata.location} </br> bbtype: ${metadata.bbtype} </br> wfreq: ${metadata.wfreq}`;
     //d3.selectAll("#selDataset").on("change", updatePlotly);
 
     // This function is called when a dropdown menu item is selected
@@ -68,11 +83,12 @@ function initMyPage(data) {
       
     // }
 }
-
+let info = document.getElementById("sample-metadata");
 function optionChanged(value){
   let values=globalData.samples[value]["sample_values"].reverse()
   let otu_ids=globalData.samples[value]["otu_ids"].reverse()
   let toolTip=globalData.samples[value]["otu_labels"].reverse()
+  let metadata = globalData.metadata[value]
 
   x = values;
   y = otu_ids.map(row=>"OTU "+row);
@@ -81,6 +97,7 @@ function optionChanged(value){
   Plotly.restyle("bar", "y", [y]);
   Plotly.restyle("bubble", "x", [otu_ids]);
   Plotly.restyle("bubble", "y", [values]);
+ info.innerHTML = `id: ${metadata.id} </br> ethnicity: ${metadata.ethnicity}`;
 }
 
 
